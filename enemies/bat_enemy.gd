32e75a2c9cbd9abcb67950225377afe49c37fdf9
DiscM <fusionfall.salabao@gmail.com>
2025-08-17 23:38:35 -0700

    Added in LoS for bat enemies & fixed collision
    Raycast2D for LoS for bats. Optimized expressions for better animation handling. Changed collision layers & masks to prevent raycast overlap with every hitbox. Designated 3 collision layers/masks.

--- enemies/bat_enemy.gd
+++ enemies/bat_enemy.gd
@@ -2,11 +2,12 @@ extends CharacterBody2D
 
 const SPEED = 30.0 
 
-@export var range : = 128
+@export var range : = 125.0
 
 @onready var sprite_2d: Sprite2D = $Sprite2D
 @onready var animation_tree: AnimationTree = $AnimationTree
 @onready var playback = animation_tree.get("parameters/StateMachine/playback") as AnimationNodeStateMachinePlayback
+@onready var ray_cast_2d: RayCast2D = $RayCast2D
 
 func _physics_process(delta: float) -> void:
 	var state = playback.get_current_node()
@@ -33,3 +34,10 @@ func is_player_in_range() -> bool:
 		if distance_to_player < range: 
 			result = true
 	return result
+	
+func can_see_player() -> bool:
+	if not is_player_in_range(): return false
+	var player: = get_player()
+	ray_cast_2d.target_position = player.global_position - global_position
+	var has_los_to_player: = not ray_cast_2d.is_colliding()
+	return has_los_to_player
