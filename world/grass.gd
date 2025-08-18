e3258a7f5d4bf836e9c7429e76ba23c5b179b21a
DiscM <fusionfall.salabao@gmail.com>
2025-08-18 12:09:45 -0700

    Added in Hitbox & Hurtbox
    Player/Grass/Bat Enemy Hitboxes and Hurtboxes have been added. Added in two new layer masks for the collisions so that the bats don't self-destruct upon touching grass. Bats can now be destroyed.

--- world/grass.gd
+++ world/grass.gd
@@ -1,11 +1,11 @@
 extends Node2D
-@onready var area_2d: Area2D = $Area2D
 @export var GRASS_EFFECT: PackedScene
+@onready var hurtbox: Hurtbox = $Hurtbox
 
 func _ready() -> void:
-	area_2d.area_entered.connect(_on_area_2d_area_entered)
+	hurtbox.hurt.connect(_on_hurt)
 
-func _on_area_2d_area_entered(other_area_2d: Area2D) -> void:
+func _on_hurt(other_hitbox: Hitbox) -> void:
 	var grass_effect_instance = GRASS_EFFECT.instantiate()
 	get_tree().current_scene.add_child(grass_effect_instance)
 	grass_effect_instance.global_position = global_position
